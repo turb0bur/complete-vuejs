3271c469e3de94816978d0dc8a3f095fc2183f52
Volodymyr Butko <turb0bur@ukr.net>
2019-10-07 23:06:45 +0300

    Set up Bootstrap 3 styles and scripts; Added Header component

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,39 @@
 <template>
+  <nav class="navbar navbar-default">
+    <div class="container-fluid">
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <router-link to="/" class="navbar-brand">Stock Trader</router-link>
+      </div>
 
+      <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+        <ul class="nav navbar-nav">
+          <router-link to="/portfolio" active-class="active" tag="li"><a>Portfolio</a></router-link>
+          <router-link to="/stocks" active-class="active" tag="li"><a>Stocks</a></router-link>
+        </ul>
+        <ul class="nav navbar-nav navbar-right">
+          <li><a href="#">End Day</a></li>
+          <li class="dropdown">
+            <a href="#"
+               class="dropdown-toggle"
+               data-toggle="dropdown"
+               role="button"
+               aria-haspopup="true"
+               aria-expanded="false">Save/Load<span class="caret"></span></a>
+            <ul class="dropdown-menu">
+              <li><a href="#">Save Data</a></li>
+              <li><a href="#">Load Data</a></li>
+            </ul>
+          </li>
+        </ul>
+      </div><!-- /.navbar-collapse -->
+    </div><!-- /.container-fluid -->
+  </nav>
 </template>
 
 <script>
