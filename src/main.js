75a92bf38c9c5fcf6ef2f666d69da33b25e29954
Volodymyr Butko <turb0bur@ukr.net>
2019-10-13 13:10:28 +0300

    Added request and response interceptors and eject them;

--- src/main.js
+++ src/main.js
@@ -4,9 +4,23 @@ import axios  from 'axios'
 import router from './router'
 import store  from './store'
 
-axios.defaults.baseURL = 'https://vuejs-learning-4d547.firebaseio.com';
+axios.defaults.baseURL                         = 'https://vuejs-learning-4d547.firebaseio.com';
 axios.defaults.headers.common['Authorization'] = 'qwerty';
-axios.defaults.headers.get['Accepts'] = 'application/json';
+axios.defaults.headers.get['Accepts']          = 'application/json';
+
+const requestInterceptor = axios.interceptors.request.use(config => {
+  console.log('Request interceptor', config);
+
+  return config;
+});
+
+const responseInterceptor = axios.interceptors.response.use(response => {
+  console.log('Response interceptor', response);
+
+  return response;
+});
+axios.interceptors.request.eject(requestInterceptor);
+axios.interceptors.response.eject(responseInterceptor);
 
 new Vue({
   el:     '#app',
