04301de8150958f27150e60785f0be45835c6b1b
Volodymyr Butko <turb0bur@ukr.net>
2019-10-13 20:44:11 +0300

    Implemented sign in process with Firebase backend

--- src/components/auth/signin.vue
+++ src/components/auth/signin.vue
@@ -4,17 +4,11 @@
       <form @submit.prevent="onSubmit">
         <div class="input">
           <label for="email">Mail</label>
-          <input
-                  type="email"
-                  id="email"
-                  v-model="email">
+          <input type="email" id="email" v-model="email">
         </div>
         <div class="input">
           <label for="password">Password</label>
-          <input
-                  type="password"
-                  id="password"
-                  v-model="password">
+          <input type="password" id="password" v-model="password">
         </div>
         <div class="submit">
           <button type="submit">Submit</button>
@@ -25,32 +19,41 @@
 </template>
 
 <script>
-  export default {
-    data () {
-      return {
-        email: '',
-        password: ''
-      }
-    },
-    methods: {
-      onSubmit () {
-        const formData = {
-          email: this.email,
-          password: this.password,
+    import axios from '../../axios-auth'
+
+    export default {
+        data() {
+            return {
+                email:    '',
+                password: ''
+            }
+        },
+        methods: {
+            onSubmit() {
+                const formData = {
+                    email:    this.email,
+                    password: this.password,
+                };
+                console.log(formData);
+                axios.post('/accounts:signInWithPassword?key=AIzaSyC9LMd_cDiHTpdysIvHI-ARuK_KftPPkGg', {
+                    email:             formData.email,
+                    password:          formData.password,
+                    returnSecureToken: true
+                })
+                    .then(response => console.log(response))
+                    .catch(error => console.error(error))
+            }
         }
-        console.log(formData)
-      }
     }
-  }
 </script>
 
 <style scoped>
   .signin-form {
     width: 400px;
     margin: 30px auto;
-    border: 1px solid #eee;
+    border: 1px solid #eeeeee;
     padding: 20px;
-    box-shadow: 0 2px 3px #ccc;
+    box-shadow: 0 2px 3px #cccccc;
   }
 
   .input {
@@ -68,13 +71,13 @@
     width: 100%;
     padding: 6px 12px;
     box-sizing: border-box;
-    border: 1px solid #ccc;
+    border: 1px solid #cccccc;
   }
 
   .input input:focus {
     outline: none;
     border: 1px solid #521751;
-    background-color: #eee;
+    background-color: #eeeeee;
   }
 
   .submit button {
@@ -94,9 +97,9 @@
   .submit button[disabled],
   .submit button[disabled]:hover,
   .submit button[disabled]:active {
-    border: 1px solid #ccc;
+    border: 1px solid #cccccc;
     background-color: transparent;
-    color: #ccc;
+    color: #cccccc;
     cursor: not-allowed;
   }
 </style>
